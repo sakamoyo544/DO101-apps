dab5d37287d1df67057359e62c1dd1c1aeff919a
sakamoyo544 <sakamoyo1227@gmail.com>
2020-05-26 01:06:26 +0900

    jika

--- weather/routes/index.js
+++ weather/routes/index.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res) {
 
 router.post('/get_weather', async function (req,res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${UNITS}&appid=${OWM_API_KEY}`;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${UNITS}&appid=a13a96d491a5ebeca1f825174a600aec`;
 
   try {
     let data = await fetch(url);
